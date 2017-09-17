9c685e36e10f242a6efb97589032f016de60245a
ipipip1735 <ipipip1735@163.com>
2017-09-18 05:54:01 +0800

    modify module bluetooth
    update the module bluetooth

--- bluetooth/src/main/java/mine/bluetooth/MainActivity.java
+++ bluetooth/src/main/java/mine/bluetooth/MainActivity.java
@@ -1,13 +1,54 @@
 package mine.bluetooth;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_ENABLE_BT = 1;
+//    ArrayAdapter mArrayAdapter = new ArrayAdapter(this, )
+
+    private final BroadcastReceiver mReceiverFound = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            System.out.println("**********  BRFound.onReceive  ***********");
+
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+//                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                System.out.println("name is " + device.getName());
+                System.out.println("address is " + device.getAddress());
+            }
+        }
+    };
+
+
+    private final BroadcastReceiver mReceiverStarted = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            System.out.println("**********  BRStarted.onReceive  ***********");
+        }
+    };
+
+    private final BroadcastReceiver mReceiverFinished = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            System.out.println("**********  BRFinished.onReceive  ***********");
+
+        }
+    };
+
 
 
     @Override
@@ -62,6 +103,14 @@ public class MainActivity extends AppCompatActivity {
         super.onBackPressed();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        System.out.println("**********  Example.onActivityResult  ***********");
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println("requestCode is " + requestCode);
+        System.out.println("resultCode is " + resultCode);
+        System.out.println("data is " + data);
+    }
 
     @Override
     protected void onStop() {
@@ -83,35 +132,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
 // button event functions
 
-
-
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     public void init(View view) {
 
         System.out.println("------>>  init  <<------");
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
         if (mBluetoothAdapter == null) {
             // Device does not support Bluetooth
             System.out.println("Device does not support Bluetooth");
         }
 
         if (!mBluetoothAdapter.isEnabled()) {
-
+            System.out.println("Would you like enable your bluetooth?");
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
 
+
     }
+
     public void start(View view) {
         System.out.println("------>>  start  <<------");
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+//                    mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                System.out.println("name is " + device.getName());
+                System.out.println("address is " + device.getAddress());
+            }
+        }
 
     }
+
     public void restart(View view) {
         System.out.println("------>>  restart  <<------");
+        if(mBluetoothAdapter.startDiscovery()) {
+            System.out.println("scaning blue tooth is true");
+        }
 
     }
+
     public void stop(View view) {
         System.out.println("------>>  stop  <<------");
 
@@ -121,11 +186,28 @@ public class MainActivity extends AppCompatActivity {
     public void bind(View view) {
         System.out.println("------>>  bind  <<------");
 
-    }
-    public void unbind(View view) {
-        System.out.println("------>>  unbind  <<------");
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiverFound, filter); // Don't forget to unregister during onDestroy
+
+
+        IntentFilter filterStarted = new IntentFilter(mBluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        registerReceiver(mReceiverStarted, filterStarted); // Don't forget to unregister during onDestroy
+
+
+        IntentFilter filterFinished = new IntentFilter(mBluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(mReceiverStarted, filterFinished); // Don't forget to unregister during onDestroy
+
 
     }
+
+    public void unbind(View view) {
+        System.out.println("------>>  unbind  <<------");
+        unregisterReceiver(mReceiverFound);
+        unregisterReceiver(mReceiverFinished);
+        unregisterReceiver(mReceiverStarted);
+
+    }
+
     public void info(View view) {
         System.out.println("------>>  info  <<------");
 
